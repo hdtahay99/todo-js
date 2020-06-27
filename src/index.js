185dfa087ec659c00657b820e63f7d076aba24e1
Heansell Tahay <hdtahay@gmail.com>
2020-06-27 12:30:19 -0600

    proyecto todo-js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 
-import { saludar } from './js/componentes';
 import './style.css';
+import {TodoList, Todo} from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-const nombre = 'Heansell Tahay';
+export const todoList = new TodoList();
 
-saludar(nombre);
+
+// localStorage.setItem('mi-key', 'ABC123');
+// sessionStorage.setItem('mi-key', 'ABC1234');
+
+// setTimeout(() => {
+//     localStorage.removeItem('mi-key');
+// }, 1500);
+
+// todoList.todos.forEach(todo => crearTodoHtml (todo) );
+todoList.todos.forEach( crearTodoHtml );
+
